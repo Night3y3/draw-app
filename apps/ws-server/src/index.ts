c77bbe533f7204dfdc30303fcba2542c23f2228c
Ast-Sabuj <sghosh@apnasanatan.com>
2025-02-27 06:53:39 +0530

    feat(ws-server): implement JWT authentication for WebSocket connections and add jsonwebtoken dependency

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,13 +1,32 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
+import { IncomingMessage } from "http";
+import { verify } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
+wss.on(
+  "connection",
+  function connection(ws: WebSocket, request: IncomingMessage) {
+    const url = request.url;
+    if (!url) {
+      return;
+    }
 
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-  });
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") ?? "";
 
-  ws.send("something");
-});
+    const decoded = verify(token, JWT_SECRET);
+
+    if (!decoded || typeof decoded === "string" || !decoded.userId) {
+      ws.close();
+      return;
+    }
+
+    ws.on("message", function message(data) {
+      console.log("received: %s", data);
+    });
+
+    ws.send("something");
+  }
+);
