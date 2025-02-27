88418b0eb2080dc1439ca122557c567d3e60f029
Ast-Sabuj <sghosh@apnasanatan.com>
2025-02-27 08:07:09 +0530

    feat: add common package with user schemas and update auth controller for validation

--- apps/http-server/src/controller/auth.controller.ts
+++ apps/http-server/src/controller/auth.controller.ts
@@ -1,15 +1,26 @@
 import { Request, Response } from "express";
 import { JwtPayload, sign, SignOptions } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateUserSchema, SignInSchema } from "@repo/common/types";
 
 export const SignUp = async (req: Request, res: Response): Promise<void> => {
-  //db logic
+  const data = CreateUserSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({ message: "Incorrect Types" });
+    return;
+  }
 
   res.status(200).send({ message: "User created" });
 };
 
 export const SignIn = async (req: Request, res: Response): Promise<void> => {
-  //db logic
+  const data = SignInSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({ message: "Incorrect Types" });
+    return;
+  }
 
   const userId = "1234";
 
